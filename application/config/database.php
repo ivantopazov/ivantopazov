21ae581d81b11794b10dbf07a0b4c9ea30f0d5a8
gittitanweb <info@titanweb.org>
2019-08-04 07:17:31 +0300

    Обновления конфигурации БД

--- application/config/database.php
+++ application/config/database.php
@@ -5,52 +5,77 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-if( $_SERVER['SERVER_NAME'] === 'direligt.beget.tech' ){
-    $db['default'] = array(
-        'dsn'	=> '',
-        'hostname' => 'localhost',
-        'username' => 'direligt_test',
-        'password' => '123456',
-        'database' => 'direligt_test',
-        'dbdriver' => 'mysqli',
-        'dbprefix' => '',
-        'pconnect' => TRUE,
-        'db_debug' => (ENVIRONMENT !== 'production'),
-        'cache_on' => TRUE,
-        'cachedir' => '',
-        'char_set' => 'utf8',
-        'dbcollat' => 'utf8_general_ci',
-        'swap_pre' => '',
-        'encrypt' => FALSE,
-        'compress' => FALSE,
-        'stricton' => FALSE,
-        'failover' => array(),
-        'save_queries' => FALSE
-    );
-}else{
-    $db['default'] = array(
-        'dsn'	=> '',
-        'hostname' => 'localhost',
-        'username' => 'direligt_yuveli',
-        'password' => '7*SP2E5#',
-        'database' => 'direligt_yuveli',
-        'dbdriver' => 'mysqli',
-        'dbprefix' => '',
-        'pconnect' => TRUE,
-        'db_debug' => (ENVIRONMENT !== 'production'),
-        'cache_on' => TRUE,
-        'cachedir' => '',
-        'char_set' => 'utf8',
-        'dbcollat' => 'utf8_general_ci',
-        'swap_pre' => '',
-        'encrypt' => FALSE,
-        'compress' => FALSE,
-        'stricton' => FALSE,
-        'failover' => array(),
-        'save_queries' => FALSE
-    );
+switch ( $_SERVER['REMOTE_ADDR'] ) {
+
+    case '127.0.0.1':
+            $db['default'] = array(
+                'dsn'	=> '',
+                'hostname' => 'localhost',
+                'username' => 'root',
+                'password' => '',
+                'database' => 'ivantopazov',
+                'dbdriver' => 'mysqli',
+                'dbprefix' => '',
+                'pconnect' => FALSE,
+                'db_debug' => (ENVIRONMENT !== 'production'),
+                'cache_on' => FALSE,
+                'cachedir' => '',
+                'char_set' => 'utf8',
+                'dbcollat' => 'utf8_general_ci',
+                'swap_pre' => '',
+                'encrypt' => FALSE,
+                'compress' => FALSE,
+                'stricton' => FALSE,
+                'failover' => array(),
+                'save_queries' => FALSE
+            );
+        break;
+
+    case 'direligt.beget.tech':
+            $db['default'] = array(
+                'dsn'	=> '',
+                'hostname' => 'localhost',
+                'username' => 'direligt_test',
+                'password' => '123456',
+                'database' => 'direligt_test',
+                'dbdriver' => 'mysqli',
+                'dbprefix' => '',
+                'pconnect' => TRUE,
+                'db_debug' => (ENVIRONMENT !== 'production'),
+                'cache_on' => TRUE,
+                'cachedir' => '',
+                'char_set' => 'utf8',
+                'dbcollat' => 'utf8_general_ci',
+                'swap_pre' => '',
+                'encrypt' => FALSE,
+                'compress' => FALSE,
+                'stricton' => FALSE,
+                'failover' => array(),
+                'save_queries' => FALSE
+            );
+        break;
+
+    default:
+            $db['default'] = array(
+                'dsn'	=> '',
+                'hostname' => 'localhost',
+                'username' => 'direligt_yuveli',
+                'password' => '7*SP2E5#',
+                'database' => 'direligt_yuveli',
+                'dbdriver' => 'mysqli',
+                'dbprefix' => '',
+                'pconnect' => TRUE,
+                'db_debug' => (ENVIRONMENT !== 'production'),
+                'cache_on' => TRUE,
+                'cachedir' => '',
+                'char_set' => 'utf8',
+                'dbcollat' => 'utf8_general_ci',
+                'swap_pre' => '',
+                'encrypt' => FALSE,
+                'compress' => FALSE,
+                'stricton' => FALSE,
+                'failover' => array(),
+                'save_queries' => FALSE
+            );
+        break;
 }
-
-
-
-
