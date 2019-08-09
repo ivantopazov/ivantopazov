641f0acf1b4ae1287ff6cad806e87e39bf047193
Kolen Val <43451550+gittitanweb@users.noreply.github.com>
2019-08-09 17:54:10 +0300

    обновил минимальную сумму заказа

--- addons/scripts/main.js
+++ addons/scripts/main.js
@@ -24,14 +24,14 @@ window.MAIN = {
                 var count = Number( count + val.qty );
             }
             var summaRUB = Number( summaCop / 100 );
-            if( summaCop > 700000 ){
+            if( summaCop > 500000 ){
                 var t = '';
                 t += 'Вы получаете бесплатную доставку';
                 $('#DeliverInfo').html( t );
                 $('#DeliverInfo').removeClass('hidden');
             }else{
                 var t = '';
-                t += 'Получите бесплатную доставку при заказе от 7000 рублей! ';
+                t += 'Получите бесплатную доставку при заказе от 5000 рублей! ';
                 t += 'Сумма вашей корзины: ' + summaRUB + ' руб.';
                 $('#DeliverInfo').html( t );
                 $('#DeliverInfo').removeClass('hidden');
@@ -430,6 +430,7 @@ window.MAIN = {
         }
 
     }
+
 };
 
 
